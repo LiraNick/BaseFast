dee860430d68a9d64db9271284b957118e94009e
Nicolas Lira Galiano <nicolas.galiano@proway.treina>
2023-02-02 19:54:02 -0300

    teste de commmit
    escrevi um comentario na main, para testar a descrição no commit

--- FST/main.py
+++ FST/main.py
@@ -1,71 +1,70 @@
-from typing import Union
+from fastapi import FastAPI
 
-from fastapi import FastAPI, HTTPException
-
-from pydantic import BaseModel
+from fastapi import HTTPException
 
 from fastapi import status
 
+from models import Aluno
+
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.get("/")
+@app.get('/')
 async def raiz():
-    return {"Hello": "word"}
+    return {"mensagem": "hello my friende"}
 
 alunos = {
 
-    1: {
-        "nome" : "Lirinha",
-        "idade" : 19,
-        "email" : "nick@gmail"
-        },
-    2: {
-        "nome" : "Jeffinho",
-        "idade" : 23,
-        "email" : "jeffbala@gmail"
-        },
-    3: {
-        "nome" : "Cleitinho",
-        "idade" : 32,
-        "email" : "cleitola@gmail"
-        },
-    4: {
-        "nome" : "Jasinto",
-        "idade" : 16,
-        "email" : "jaja@gmail"
-        }
+    1: {"nome" : "Lirinha", "idade" : 19, "email" : "nickporr@gmail"},
+    2: {"nome" : "jasinto", "idade" : 12, "email" : "leite@gmail"},
+    3: {"nome" : "tia", "idade" : 33, "email" : "romba@gmail"},
+    4: {"nome" : "jeff", "idade" : 36, "email" : "jeffbala@gmail"}
 
 }
-
 
 @app.get('/alunos')
 async def get_alunos():
     return alunos
 
-@app.get("/alunos/{alunos_id}")
-async def get_aluno(alunos_id: int):
+@app.get('/alunos')
+async def get_alunos():
+    return alunos
+
+#testanto commit
+@app.get('/alunos/{alunos_id}')
+async def read_item(alunos_id: int):
     aluno = alunos[alunos_id]
-    alunos.update({"id" : alunos_id})
+    alunos.update({'id': alunos_id})
+
+    return aluno 
+
+@app.get('/alunos/{alunos_id}')
+async def read_item(alunos_id: int):
     try:
         aluno = alunos[alunos_id]
         alunos.update({'id':alunos_id})
-        return pessoa
+        return aluno
     except KeyError:
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, detail = 'aluno não encontrado')
+            status_code = status.HTTP_404_NOT_FOUND, detail = 'Não encontramos'
+        )
+
+@app.post("/alunos", status_code=status.HTTP_201_CREATED)
+async def post_aluno(aluno: Aluno):
+    next_id: int = len(alunos) + 1
+    alunos[next_id] = aluno
+    del aluno.id
     return aluno
 
+@app.put('/alunos/{alunos_id}')
+async def put_aluno(aluno_id: int, aluno: Aluno):
+    if aluno_id in alunos:
+        alunos[aluno_id] = aluno
+        return aluno
+    else:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = 'Não encontramos')
 
-
-
-
-if __name__ == '__main__':
-    import uvicorn 
+if __name__ == 'main':
+    import uvicorn
 
     uvicorn.run(
         "main:app",
@@ -73,5 +72,5 @@ if __name__ == '__main__':
         port = 8000,
         log_level = "info",
         reload = True
-    )
 
+    )
